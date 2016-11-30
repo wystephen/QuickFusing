ef76db0a259387f971ae5dca8e3257c952e71ce1
steve <551619855@qq.com>
2016-11-30 11:27:02 +0800

    isnan => std::isnan.

--- ResultEvaluation.cpp
+++ ResultEvaluation.cpp
@@ -119,93 +119,93 @@ int main(int argc,char *argv[])
     }
 
     std::ofstream outf("tmp_file_dir/keypoint.csv");
-//
-//    outf << "0.8,-5.6,"<<std::to_string(ImuData(0,0))<<std::endl;
-//
-//    std::vector<double> rx,ry,rt;
-//    rx.push_back(0.8);
-//    ry.push_back(-5.6);
-//    rt.push_back(double(ImuData(1086,0)));
-//
-//    rx.push_back(-8.0);
-//    ry.push_back(-5.6);
-//    rt.push_back(double(ImuData(2144,0)));
-//
-//    rx.push_back(-8.0);
-//    ry.push_back(-2.4);
-//    rt.push_back(double(ImuData(2700,0)));
-//
-//    rx.push_back(0.8);
-//    ry.push_back(-2.4);
-//    rt.push_back(double(ImuData(3924,0)));
-//
-//    rx.push_back(0.8);
-//    ry.push_back(2.4);
-//    rt.push_back(ImuData(4648,0));
-//
-//    rx.push_back(-8.0);
-//    ry.push_back(2.4);
-//    rt.push_back(ImuData(6025,0));
-//
-//    rx.push_back(-8.0);
-//    ry.push_back(5.6);
-//    rt.push_back(ImuData(6679,0));
-//
-//    rx.push_back(0.8);
-//    ry.push_back(5.6);
-//    rt.push_back(ImuData(7806,0));
-//
-//    rx.push_back(0.8);
-//    ry.push_back(-5.6);
-//    rt.push_back(ImuData(9614,0));
-//
-//
-//    rx.push_back(-8.0);
-//    ry.push_back(-5.6);
-//    rt.push_back(ImuData(10811,0));
-//
-//    rx.push_back(-8.0);
-//    ry.push_back(-2.4);
-//    rt.push_back(ImuData(11423,0));
-//
-//    rx.push_back(0.8);
-//    ry.push_back(-2.4);
-//    rt.push_back(ImuData(12550,0));
-//
-//    rx.push_back(0.8);
-//    ry.push_back(2.4);
-//    rt.push_back(ImuData(13468,0));
-//
-//
-//    rx.push_back(-8.0);
-//    ry.push_back(2.4);
-//    rt.push_back(ImuData(14581,0));
-//
-//    rx.push_back(-8.0);
-//    ry.push_back(5.6);
-//    rt.push_back(ImuData(15290,0));
-//
-//    rx.push_back(0.8);
-//    ry.push_back(5.6);
-//    rt.push_back(ImuData(16584,0));
-//
-//    rx.push_back(0.8);
-//    ry.push_back(-5.6);
-//    rt.push_back(ImuData(18518,0));
-//
-//
-//    for(int i(0);i<rx.size();++i)
-//    {
-//        outf << std::to_string(rx[i]) << ","
-//                                      <<std::to_string(ry[i])
-//                                      << ","
-//                                      <<std::to_string(rt[i])
-//                                      <<std::endl;
-//    }
-//
-//    std::cout << "0.8,-5.6,"<<std::to_string(ImuData(19297,0))<<std::endl;
-//
-//    outf.close();
+
+    outf << "0.8,-5.6,"<<std::to_string(ImuData(0,0))<<std::endl;
+
+    std::vector<double> rx,ry,rt;
+    rx.push_back(0.8);
+    ry.push_back(-5.6);
+    rt.push_back(double(ImuData(1086,0)));
+
+    rx.push_back(-8.0);
+    ry.push_back(-5.6);
+    rt.push_back(double(ImuData(2144,0)));
+
+    rx.push_back(-8.0);
+    ry.push_back(-2.4);
+    rt.push_back(double(ImuData(2700,0)));
+
+    rx.push_back(0.8);
+    ry.push_back(-2.4);
+    rt.push_back(double(ImuData(3924,0)));
+
+    rx.push_back(0.8);
+    ry.push_back(2.4);
+    rt.push_back(ImuData(4648,0));
+
+    rx.push_back(-8.0);
+    ry.push_back(2.4);
+    rt.push_back(ImuData(6025,0));
+
+    rx.push_back(-8.0);
+    ry.push_back(5.6);
+    rt.push_back(ImuData(6679,0));
+
+    rx.push_back(0.8);
+    ry.push_back(5.6);
+    rt.push_back(ImuData(7806,0));
+
+    rx.push_back(0.8);
+    ry.push_back(-5.6);
+    rt.push_back(ImuData(9614,0));
+
+
+    rx.push_back(-8.0);
+    ry.push_back(-5.6);
+    rt.push_back(ImuData(10811,0));
+
+    rx.push_back(-8.0);
+    ry.push_back(-2.4);
+    rt.push_back(ImuData(11423,0));
+
+    rx.push_back(0.8);
+    ry.push_back(-2.4);
+    rt.push_back(ImuData(12550,0));
+
+    rx.push_back(0.8);
+    ry.push_back(2.4);
+    rt.push_back(ImuData(13468,0));
+
+
+    rx.push_back(-8.0);
+    ry.push_back(2.4);
+    rt.push_back(ImuData(14581,0));
+
+    rx.push_back(-8.0);
+    ry.push_back(5.6);
+    rt.push_back(ImuData(15290,0));
+
+    rx.push_back(0.8);
+    ry.push_back(5.6);
+    rt.push_back(ImuData(16584,0));
+
+    rx.push_back(0.8);
+    ry.push_back(-5.6);
+    rt.push_back(ImuData(18518,0));
+
+
+    for(int i(0);i<rx.size();++i)
+    {
+        outf << std::to_string(rx[i]) << ","
+                                      <<std::to_string(ry[i])
+                                      << ","
+                                      <<std::to_string(rt[i])
+                                      <<std::endl;
+    }
+
+    std::cout << "0.8,-5.6,"<<std::to_string(ImuData(19297,0))<<std::endl;
+
+    outf.close();
 
     std::vector<double> imut;
     for(int k(0);k<ImuData.rows();++k)

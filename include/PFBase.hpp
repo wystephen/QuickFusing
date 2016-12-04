5330171e3906be8e6593e57f19d1e2159f41e242
steve <551619855@qq.com>
2016-12-04 10:24:11 +0800

    0000

---
+++ include/PFBase.hpp
@@ -0,0 +1,10 @@
+#pragma once
+//Create by steve in 16-12-4 at 上午10:23
+//
+// Created by steve on 16-12-4.
+//
+
+#ifndef QUICKFUSING_PFBASE_HPP
+#define QUICKFUSING_PFBASE_HPP
+
+#endif //QUICKFUSING_PFBASE_HPP

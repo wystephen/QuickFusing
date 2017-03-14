011e58c9d3ae1683d262a7679e3ee2c9c4406157
steve <551619855@qq.com>
2017-03-14 20:42:48 +0800

    desktop modified all files.
    Signed-off-by: steve <551619855@qq.com>

--- ResultData/test_list.py
+++ ResultData/test_list.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     # for exam_id,particle_num,t_sigma,eval_sigma in ([3,5],[1,2,3,4],[0.1,0.3,0.5],[0.1,0.5,10]):
     #     print(exam_id,particle_num,t_sigma,eval_sigma)
     for exam_id in [3, 5]:
-        for particle_num in [3000, 5000, 8000, 10000, 20000]:
-            for t_sigma in [1.0]:
+        for particle_num in [3000, 5000, 8000, 10000, 15000, 20000, 30000]:
+            for t_sigma in [5.0, 10.0]:
                 for eval_sigma in [1.0]:
-                    for times in range(1, 5):
+                    for times in range(1, 15):
                         print(exam_id, particle_num, t_sigma, eval_sigma, times)
                         file_name = "/home/steve/locate/RunResult/{0}/{1}-{2}-{3}/{4}-".format(
                             exam_id,

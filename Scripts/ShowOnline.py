f2d488444b3677bafd31ca3c13f958e095931827
steve <551619855@qq.com>
2017-07-07 17:29:58 +0800

    try to modified the time stamp in uwbresult.csv...

--- Scripts/ShowOnline.py
+++ Scripts/ShowOnline.py
@@ -5,10 +5,11 @@ import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
 import time
-import  os
+import os
+
 
 class RealTimePlot:
-    def __init__(self,file_name):
+    def __init__(self, file_name):
         self.file_name = file_name
         self.n_time = os.stat(file_name).st_mtime
 
@@ -16,22 +17,23 @@ class RealTimePlot:
         self.ax = p3.Axes3D(self.fig)
 
         self.data = np.loadtxt(self.file_name)
-        self.ax.plot(self.data[:,0],self.data[:,1],self.data[:,2],'r*-')
+        self.ax.plot(self.data[:, 0], self.data[:, 1], self.data[:, 2], 'r*-')
 
     def start(self):
         line_ani = animation.FuncAnimation(
-            self.fig,self.update_trace,interval=100
+            self.fig, self.update_trace, interval=100
         )
         plt.show()
 
-    def update_trace(self,data):
-        if(os.stat(file_name).st_mtime>self.n_time):
+    def update_trace(self, data):
+        if (os.stat(file_name).st_mtime > self.n_time):
             time.sleep(1)
             self.n_time = os.stat(self.file_name).st_mtime
             self.data = np.loadtxt(self.file_name)
             self.ax.clear()
 
-            self.ax.plot(self.data[:,0],self.data[:,1],self.data[:,2],'r*-')
+            self.ax.plot(self.data[:, 0], self.data[:, 1], self.data[:, 2], 'r*-')
+
 
 if __name__ == '__main__':
     file_name = '../ResultData/imu.txt'

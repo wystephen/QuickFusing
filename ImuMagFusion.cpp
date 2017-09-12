4b5c8a585e48bcee52f454bf4bdc0b19443b045f
steve <551619855@qq.com>
2017-09-12 19:21:40 +0800

    ...ugly error...

--- ImuMagFusion.cpp
+++ ImuMagFusion.cpp
@@ -24,7 +24,7 @@
 
 
 #include "MYEKF.h"
-//#include "Zero_Detecter.h"
+#include "Zero_Detecter.h"
 #include<Eigen/Dense>
 #include <Eigen/Geometry>
 //#include <sophus/se3.h>
@@ -92,61 +92,7 @@ G2O_USE_TYPE_GROUP(slam3d)
 
 namespace plt = matplotlibcpp;
 
-bool GLRT_Detector(Eigen::MatrixXd u,
-                   const SettingPara &para_) {
-    Eigen::Vector3d ya_m;
-    double g = para_.gravity_;
 
-    double T(0.0);
-//    Eigen::MatrixXd Tmatrix(1, 1);
-//    Tmatrix
-
-    for (int i(0); i < 3; ++i) {
-        ya_m(i) = u.block(i, 0, 1, u.cols()).mean();
-    }
-
-    Eigen::Vector3d tmp;
-
-    for (int i(0); i < u.cols(); ++i) {
-
-        tmp = u.block(0, i, 3, 1) - g * ya_m / ya_m.norm();
-        if (std::isnan(tmp.sum())) {
-            std::cout << "nan at tmp in " << __FUNCTION__ << ":"
-                      << __FILE__ << ":" << __LINE__ << std::endl;
-        }
-
-//        std::cout << " u block size : " << u.block(3,i,3,1).rows()<< std::endl;
-//        std::cout << "tmp size :" << tmp.rows()<< std::endl;
-
-        T += (u.block(3, i, 3, 1).transpose() * u.block(3, i, 3, 1) / para_.sigma_g_ +
-              tmp.transpose() * tmp / para_.sigma_a_).sum();
-
-//        if(std::isnan(Tmatrix.sum()))
-//        {
-//            std::cout << "Tmatrix is nan" << __FILE__
-//                                          << ":"<< __LINE__ << std::endl;
-//        }
-
-
-    }
-
-//    if (Tmatrix.size() != 1) {
-//        MYERROR("Tmatrxi size is not equal to 1")
-//    }
-
-
-    T = T / double(para_.ZeroDetectorWindowSize_);
-
-//    std::cout << "T :" << T << std::endl;
-    if (T < para_.gamma_) {
-        return true;
-
-    } else {
-
-        return false;
-    }
-
-}
 
 Eigen::Isometry3d tq2Transform(Eigen::Vector3d offset,
                                Eigen::Quaterniond q) {

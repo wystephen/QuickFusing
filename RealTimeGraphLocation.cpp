cd4fb0603d6e5486a1d8ca5797b02cb47ef2ed4f
steve <551619855@qq.com>
2017-09-14 10:51:33 +0800

    a preprocess is needed.

--- RealTimeGraphLocation.cpp
+++ RealTimeGraphLocation.cpp
@@ -1,6 +1,28 @@
 //
 // Created by steve on 17-9-12.
 //
+/**
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         佛祖保佑       永无BUG
+*/
 /// EIGEN
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
@@ -159,15 +181,15 @@ int main(int argc, char *argv[]) {
             uwb_raw(i, j) = *(UwbRawReader.GetMatrix()(i, j));
         }
     }
-    CppExtent::CSVReader BeaconsetReader(dir_name + "beaconset.data.csv");
-    Eigen::MatrixXd beaconset;
-    beaconset.resize(BeaconsetReader.GetMatrix().GetRows(),
-                     BeaconsetReader.GetMatrix().GetCols());
-    for (int i(0); i < beaconset.rows(); ++i) {
-        for (int j(0); j < beaconset.cols(); ++j) {
-            beaconset(i, j) = *BeaconsetReader.GetMatrix()(i, j);
-        }
-    }
+//    CppExtent::CSVReader BeaconsetReader(dir_name + "beaconset.data.csv");
+//    Eigen::MatrixXd beaconset;
+//    beaconset.resize(BeaconsetReader.GetMatrix().GetRows(),
+//                     BeaconsetReader.GetMatrix().GetCols());
+//    for (int i(0); i < beaconset.rows(); ++i) {
+//        for (int j(0); j < beaconset.cols(); ++j) {
+//            beaconset(i, j) = *BeaconsetReader.GetMatrix()(i, j);
+//        }
+//    }
 
     CppExtent::CSVReader ImuDataReader(dir_name + "sim_imu.csv");
 
@@ -246,6 +268,8 @@ int main(int argc, char *argv[]) {
     std::vector<double> gx, gy, gz;// graph_optimizer output
     std::vector<double> imu_x, imu_y, imu_z;// imu output
 
+    std::vector<int> imu_vertex_id;// save imu_data_index for each SE3 vertex.
+
 
     /**
      * Main loop
@@ -279,14 +303,14 @@ int main(int argc, char *argv[]) {
   * Spacial preprocess !!!!
   * set z of beaconset to zero
   */
-    for (int i(0); i < beaconset.rows(); ++i) {
-        beaconset(i, 2) = 0.0;
-    }
+//    for (int i(0); i < beaconset.rows(); ++i) {
+//        beaconset(i, 2) = 0.0;
+//    }
 
 
 
     /// add beacon vertex
-    for (int i(0); i < beaconset.rows(); ++i) {
+    for (int i(0); i < uwb_raw.cols() - 1; ++i) {
         auto *v = new g2o::VertexSE3();
         double p[6] = {0};
 //        for (int j(0); j < 3; ++j) {
@@ -301,26 +325,204 @@ int main(int argc, char *argv[]) {
     bool tmp_set_bool(true);// avoid the warning given by IDE...
     while (tmp_set_bool) {
         //
+        if (uwb_data_index >= uwb_raw.rows() || imu_data_index >= imu_data.rows()) {
 
-
-
-        bool zupt_flag = true;
-
-        if (imu_data_index > init_para.ZeroDetectorWindowSize_) {
-//            std::cout << "imu_data_index :" << imu_data_index << std::endl;
-            zupt_flag = GLRT_Detector(imu_data.block(imu_data_index - init_para.ZeroDetectorWindowSize_, 1,
-                                                     init_para.ZeroDetectorWindowSize_, 6).transpose().eval(),
-                                      init_para);
+            break;
         }
+
+
+        if (uwb_raw(uwb_data_index, 0) < imu_data(imu_data_index, 0)) {
+            uwb_data_index++;
+        } else {
+
+            bool zupt_flag = true;
+
+            if (imu_data_index > init_para.ZeroDetectorWindowSize_) {
+//            std::cout << "imu_data_index :" << imu_data_index << std::endl;
+                zupt_flag = GLRT_Detector(imu_data.block(imu_data_index - init_para.ZeroDetectorWindowSize_, 1,
+                                                         init_para.ZeroDetectorWindowSize_, 6).transpose().eval(),
+                                          init_para);
+            }
 //        std::cout << "zupt flag:" << zupt_flag << std::endl;
-        zupt_v.push_back(zupt_flag ? 1.0 : 0.0);
+            zupt_v.push_back(zupt_flag ? 1.0 : 0.0);
 
 
-        ///
+            auto ekf_output = myekf.GetPosition(
+                    imu_data.block(imu_data_index, 1, 1, 6).transpose(),
+                    (zupt_flag ? 1.0 : 0.0)
+            );
+
+            auto the_transform = myekf.getTransformation();
+/// generate better estimate
+
+            Eigen::Isometry3d before_state = Eigen::Isometry3d::Identity();
+            if (imu_data_index > 0) {
+                double before_data[10] = {0};
+                globalOptimizer.vertex(trace_id - 1)->getEstimateData(before_data);
+
+                Eigen::Vector3d offset(before_data[0], before_data[1], before_data[2]);
+
+                Eigen::Quaterniond qq(before_data[6], before_data[3], before_data[4], before_data[5]);
+
+                before_state = tq2Transform(offset, qq);
+
+            }
+
+            /// add vertex
+            auto *v = new g2o::VertexSE3();
+            v->setId(trace_id);
+            v->setEstimate(before_state * last_transform.inverse() * the_transform);
+
+            globalOptimizer.addVertex(v);
+
+            /// get delta theta
+            double the_theta(myekf.getOriente());
+            double delta_ori = the_theta - last_theta;
+
+            if (delta_ori > M_PI) {
+                delta_ori -= (2 * M_PI);
+            } else if (delta_ori < -M_PI) {
+                delta_ori += (2.0 * M_PI);
+            }
+            if (std::isnan(delta_ori)) {
+                delta_ori = 0.0;
+            }
+//                gekf.getDeltaOrientation();
+            bool is_corner(false);
+//                if(std::abs(the_theta-))
+            if (std::abs(delta_ori) > turn_threshold) {
+                is_corner = true;
+            }
+
+            last_theta = the_theta;
+            std::cout << "current delta theta : " << delta_ori << std::endl;
+
+//                std::cout << trace_id << " " << delta_ori << "   " << is_corner
+//                          << is_corner << is_corner << is_corner << std::endl;
+
+            ///add transform edge
+
+            if (trace_id > 0) {
+                auto *edge_se3 = new g2o::EdgeSE3();
+
+                edge_se3->vertices()[0] = globalOptimizer.vertex(trace_id - 1);
+                edge_se3->vertices()[1] = globalOptimizer.vertex(trace_id);
+
+                Eigen::Matrix<double, 6, 6> information = Eigen::Matrix<double, 6, 6>::Identity();
 
 
+                information(0, 0) = information(1, 1) = information(2, 2) = first_info;
+                information(3, 3) = information(4, 4) = information(5, 5) = second_info;
 
-        last_zupt_flag = zupt_flag;
+                if (is_corner) {
+                    information(0, 0) = information(1, 1) = information(2, 2) = first_info / corner_ratio;
+                    information(3, 3) = information(4, 4) = information(5, 5) = second_info / corner_ratio;
+                }
+
+                edge_se3->setInformation(information);
+
+                edge_se3->setMeasurement(last_transform.inverse() * the_transform);
+
+//                edge_vector.push_back(latest_transform.inverse() * the_transform);
+
+
+//                    double tmp_data[10]={0};
+//                    edge_se3->getMeasurementData(tmp_data);
+//                    for(int ti(0);ti<7;++ti)
+//                    {
+//                        out_v_before<< tmp_data[ti] << " ";
+//                    }
+//                    out_v_before << std::endl;
+
+//                out_v_before << delta_ori / 180.0 * M_PI << std::endl;
+
+                globalOptimizer.addEdge(edge_se3);
+
+
+            }
+
+            /// add range edge
+
+            //  get measurement
+            if (std::abs(uwb_raw(uwb_data_index, 0) - imu_data(imu_data_index, 0)) < 1.0) {
+
+                Eigen::VectorXd uwb_measure;
+
+                uwb_measure.resize(uwb_raw.cols() - 1);
+                uwb_measure.setZero();
+
+
+                if (uwb_data_index == 0 || uwb_data_index > uwb_raw.rows() - 3) {
+                    uwb_measure = uwb_raw.block(uwb_data_index, 1, 1, uwb_measure.rows()).transpose();
+                } else {
+                    uwb_measure += uwb_raw.block(uwb_data_index, 1, 1, uwb_measure.rows()).transpose();
+                    uwb_measure += uwb_raw.block(uwb_data_index + 1, 1, 1, uwb_measure.rows()).transpose();
+
+                    uwb_measure /= 2.0;
+                }
+
+                // build and add edge
+
+                for (int bi(0); bi < uwb_measure.rows(); ++bi) {
+                    if (bi == 10
+
+                            ) {
+                        break;
+                    }
+                    auto *dist_edge = new DistanceEdge();
+                    dist_edge->vertices()[0] = globalOptimizer.vertex(beacon_id_offset + bi);
+                    dist_edge->vertices()[1] = globalOptimizer.vertex(trace_id);
+
+                    dist_edge->setMeasurement(std::sqrt(uwb_measure(bi) * uwb_measure(bi) - z_offset * z_offset));
+
+                    Eigen::Matrix<double, 1, 1> information;
+                    information(0, 0) = distance_info;
+
+                    dist_edge->setInformation(information);
+                    dist_edge->setSigma(distance_sigma);
+                    dist_edge->setRobustKernel(new g2o::RobustKernelHuber());
+
+                    globalOptimizer.addEdge(dist_edge);
+                }
+
+                /// try online optimize
+
+                int last_offset(delay_times);
+                if (trace_id > last_offset) {
+                    globalOptimizer.vertex(trace_id - last_offset + 1)->setFixed(true);
+
+                }
+
+                if (trace_id >= out_delay_times) {
+                    double td[10] = {0};
+                    globalOptimizer.vertex(trace_id - out_delay_times)->getEstimateData(td);
+                    online_gx.push_back(td[0]);
+                    online_gy.push_back(td[1]);
+                }
+
+                if (trace_id > 10) {
+                    double time_before(TimeStamp::now());
+                    globalOptimizer.initializeOptimization();
+                    globalOptimizer.optimize(max_optimize_times);
+//                        std::cout << "One step optimize" << TimeStamp::now() - time_before << std::endl;
+
+                }
+            }
+
+            /// updata transform matrix
+            last_transform = the_transform;
+
+            /// increase trace id
+            trace_id++;
+
+
+            imu_data_index++;
+
+
+            last_zupt_flag = zupt_flag;
+        }
+
+
     }
 
 

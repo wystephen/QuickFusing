a215f91f868071cc780975b03cacb9f4d5d5e626
steve <551619855@qq.com>
2017-10-06 20:25:29 +0800

    zupt based on gtsam  preintegration and priorfactor ,good result another parameters .

--- ISAMImu.cpp
+++ ISAMImu.cpp
@@ -24,3 +24,57 @@
 // Created by steve on 17-10-6.
 //
 
+#include "CSVReader.h"
+#include "matplotlib_interface.h"
+#include "time_stamp.h"
+
+#include "SettingPara.h"
+#include "EKF.hpp"
+#include "MYEKF.h"
+#include "Zero_Detecter.h"
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+// GTSAM related includes.
+#include <gtsam/navigation/CombinedImuFactor.h>
+#include <gtsam/navigation/GPSFactor.h>
+#include <gtsam/navigation/ImuFactor.h>
+#include <gtsam/slam/dataset.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/PriorFactor.h>
+
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/inference/Symbol.h>
+
+#include <gtsam/nonlinear/ISAM2.h>
+
+
+#include <thread>
+
+using namespace gtsam;
+using namespace std;
+
+
+using symbol_shorthand::X; // Pose3 (x,y,z,r,p,y)
+using symbol_shorthand::V; // Vel   (xdot,ydot,zdot)
+using symbol_shorthand::B; // Bias  (ax,ay,az,gx,gy,gz)
+
+PreintegratedImuMeasurements *imu_preintegrated_;
+
+namespace plt = matplotlibcpp;
+
+
+Eigen::Isometry3d tq2Transform(Eigen::Vector3d offset,
+                               Eigen::Quaterniond q) {
+    Eigen::Isometry3d T;
+    T.setIdentity();
+    T.rotate(q.toRotationMatrix());
+    T(0, 3) = offset(0);
+    T(1, 3) = offset(1);
+    T(2, 3) = offset(2);
+    return T;
+}
+

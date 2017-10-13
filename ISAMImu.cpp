b66cc05b5f8ea5b76f9122cbf90d61da991c4934
steve <551619855@qq.com>
2017-10-13 21:19:15 +0800

    begin to modified the ISAMImu... debug.

--- ISAMImu.cpp
+++ ISAMImu.cpp
@@ -291,7 +291,7 @@ int main() {
 
                 if (trace_id > 100){
                     isam2.calculateEstimate().print("before update values at " + std::to_string(trace_id) + " is :");
-                    isam2.update(graph, initial_values);
+//                    isam2.update(graph, initial_values);
                     GaussNewtonOptimizer gaussNewtonOptimizer(graph,initial_values);
                     gaussNewtonOptimizer.optimizeSafely();
                     gaussNewtonOptimizer.values().print("value of " + std::to_string(trace_id) +":");

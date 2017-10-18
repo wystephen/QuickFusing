3de9c872352120e3a3040c21309336243fe7411e
steve <551619855@qq.com>
2017-10-18 09:33:45 +0800

    SC preintegration ok .~closely to right result.

--- include/OwnFactor/MagConstraintFactor.h
+++ include/OwnFactor/MagConstraintFactor.h
@@ -26,11 +26,36 @@
 
 #ifndef QUICKFUSING_MAGCONSTRAINTFACTOR_H
 #define QUICKFUSING_MAGCONSTRAINTFACTOR_H
+#include <gtsam/nonlinear/NonlinearFactor.h>
+#include <gtsam/geometry/Rot2.h>
+#include <gtsam/geometry/Rot3.h>
+
+#include <gtsam/geometry/Pose3.h>
+
+class MagConstraintFactor :public gtsam::NoiseModelFactor2<gtsam::Pose3,gtsam::Point3>{
+
+    /**
+     *  find local magnetic field at http://www.ngdc.noaa.gov/geomag-web/#igrfwmm
+     */
+    const gtsam::Point3 measured_; /// < the measured magnetomeer values
+    const gtsam::Point3 nM_; /// < local magnetic field (mag output units)
+//    const gtsam::Point3 bias_; /// < bias
+
+public:
+    MagConstraintFactor(gtsam::Key key_pose,
+    gtsam::Key key_bias,
+    const gtsam::Point3& measured,
+    const gtsam::Point3& nM,
+    const gtsam::SharedNoiseModel& model):
+            gtsam::NoiseModelFactor2<gtsam::Pose3,gtsam::Point3>(model,key_pose,key_bias),
+            measured_(measured),nM_(nM){
+
+    }
 
 
-class MagConstraintFactor {
+
+
 
 };
-
 
 #endif //QUICKFUSING_MAGCONSTRAINTFACTOR_H

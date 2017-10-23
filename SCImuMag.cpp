ab31a3b70858a409f0484221d012fcfc24e6dc53
steve <551619855@qq.com>
2017-10-23 12:36:09 +0800

    SC preintegration ok .~closely to right result. fixed a bug of magconstraintFactor.

--- SCImuMag.cpp
+++ SCImuMag.cpp
@@ -335,8 +335,8 @@ int main(int argc, char *argv[]) {
                     graph->add(PoseRotationPrior<Pose3>(
                             X(trace_id),
                             Rot3::RzRyRx(Vector3(imudata(index, 9) / 180.0 * M_PI,
-                                                 0.0,//imudata(index, 8) / 180.0 * M_PI,
-                                                 0.0)),//imudata(index, 7) / 180.0 * M_PI)),
+                                                 imudata(index, 8) / 180.0 * M_PI,
+                                                 imudata(index, 7) / 180.0 * M_PI)),
                             mag_all_noise
                     ));
                     std::cout << imudata(index, 7) << std::endl;

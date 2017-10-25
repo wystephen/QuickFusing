3d2ba555228198202b2d6ed62c8395d7b12f3470
steve <551619855@qq.com>
2017-10-25 20:09:40 +0800

    SC preintegration ok .~closely to right result. fixed a bug of MagConstraintFactor.

--- Scripts/SearchJYImuMagPara.py
+++ Scripts/SearchJYImuMagPara.py
@@ -22,33 +22,31 @@
          佛祖保佑       永无BUG 
 '''
 
-
 import sys
 import os
 import time
 import random
 
-
 import threading
+
 
 def run_the_str(str):
     print(str + 'started')
     # time.sleep(int(random.random()*3))
-    print(str+' finished.')
-
+    print(str + ' finished.')
 
 
 if __name__ == '__main__':
 
     all_str_run = list()
-    for i1 in [0.00001,0.00004, 0.0001,  0.0004,0.0056,0.001, 0.011,  0.05,  0.1, 0.3, 0.9,1.2]:
-        for i2 in [0.00001,  0.0001, 0.008, 0.02, 0.05,  0.1,  0.9,1.0]:
-            for i3 in [0.001,0.003,0.007,0.01,0.03,0.08,0.3,1.0,2.0,3.0,5.0,10.0,20.0]:
-                all_str_run.append("../cmake-build-debug/SCImuMag {0} {1} {2} ".format(
+    for i1 in [0.00001, 0.00004, 0.0001, 0.0004, 0.0056, 0.001, 0.011, 0.05, 0.1, 0.3, 0.9, 1.2]:
+        for i2 in [0.00001, 0.0001, 0.008, 0.02, 0.05, 0.1, 0.9, 1.0]:
+            for i3 in [0.001, 0.003, 0.007, 0.01, 0.03, 0.08, 0.3, 1.0, 2.0, 3.0, 5.0, 10.0, 20.0]:
+                all_str_run.append("../cmake-build-debug/JYImuMag {0} {1} {2} ".format(
                     i1, i2, i3
 
                 ))
-
+    print("finished preparing parameters!")
 
     count = 0
     while count < len(all_str_run):
@@ -57,4 +55,3 @@ if __name__ == '__main__':
             t = threading.Thread(target=run_the_str, args=(all_str_run[count],))
             count += 1
             t.start()
-

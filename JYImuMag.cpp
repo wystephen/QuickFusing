4a0555d1f64a5ab5b98360c9f174a386be6091bc
steve <551619855@qq.com>
2017-10-29 10:09:40 +0800

    zupt for JY imu , good parameters. JYImuMag With good result at parameter: ./cmake-build-debug/JYImuMag 0.000001 0.01 0.02 9.6 0.1 0.2

--- JYImuMag.cpp
+++ JYImuMag.cpp
@@ -611,17 +611,17 @@ int main(int argc, char *argv[]) {
      * Plot Trace
      */
     plt::plot(gx, gy, "r-+");
-//    plt::plot(ekfx, ekfy, "b-");
+    plt::plot(ekfx, ekfy, "b-");
     plt::title("img-sv:" + std::to_string(sv) + "sa:" + std::to_string(sa) + "-sg:" +
                std::to_string(sg)
                + "g:" + std::to_string(gravity) + "s_mag_att:" + std::to_string(smag_attitude) +
                "s_g_att:" + std::to_string(sgravity_attitude) + "initial_heading:" + std::to_string(initial_heading));
 
-    plt::save("img-sv:" + std::to_string(sv) + "sa:" + std::to_string(sa) + "-sg:" +
-              std::to_string(sg)
-              + "g:" + std::to_string(gravity) + "s_mag_att:" + std::to_string(smag_attitude) +
-              "s_g_att:" + std::to_string(sgravity_attitude) + "initial_heading:" + std::to_string(initial_heading) +
-              ".png");
+//    plt::save("img-sv:" + std::to_string(sv) + "sa:" + std::to_string(sa) + "-sg:" +
+//              std::to_string(sg)
+//              + "g:" + std::to_string(gravity) + "s_mag_att:" + std::to_string(smag_attitude) +
+//              "s_g_att:" + std::to_string(sgravity_attitude) + "initial_heading:" + std::to_string(initial_heading) +
+//              ".png");
 
 
 
@@ -629,7 +629,7 @@ int main(int argc, char *argv[]) {
 //    plt::plot(ay);
 //    plt::plot(az);
 //    plt::plot(zupt_v);
-//    plt::show();
+    plt::show();
 
 
     return 0;

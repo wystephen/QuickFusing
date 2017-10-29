3798912e1cb94eb3fae3a8d849185c7f1109a3d1
steve <551619855@qq.com>
2017-10-29 19:42:41 +0800

    double-feet constrainted zupt.

--- TwoFeetsZUPT.cpp
+++ TwoFeetsZUPT.cpp
@@ -24,3 +24,59 @@
 // Created by steve on 17-10-29.
 //
 
+#include "CSVReader.h"
+#include "matplotlib_interface.h"
+#include "time_stamp.h"
+
+#include "SettingPara.h"
+#include "EKF.hpp"
+#include "MYEKF.h"
+#include "Zero_Detecter.h"
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+// GTSAM related includes.
+#include <gtsam/navigation/CombinedImuFactor.h>
+#include <gtsam/navigation/GPSFactor.h>
+#include <gtsam/navigation/ImuFactor.h>
+#include <gtsam/slam/dataset.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/PoseRotationPrior.h>
+#include <gtsam/navigation/AttitudeFactor.h>
+#include <gtsam/navigation/AHRSFactor.h>
+#include <gtsam/navigation/ManifoldPreintegration.h>
+#include <gtsam/navigation/TangentPreintegration.h>
+#include <gtsam/slam/RangeFactor.h>
+//#indluce <gtsam
+
+
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/inference/Symbol.h>
+
+#include <gtsam/nonlinear/ISAM2.h>
+#include <gtsam/nonlinear/NonlinearISAM.h>
+
+
+#include <thread>
+#include <OwnFactor/MagConstraintFactor.h>
+
+using namespace gtsam;
+using namespace std;
+
+
+using symbol_shorthand::X; // Pose3 (x,y,z,r,p,y)
+using symbol_shorthand::V; // Vel   (xdot,ydot,zdot)
+using symbol_shorthand::B; // Bias  (ax,ay,az,gx,gy,gz)
+using symbol_shorthand::M; // Bias of Magconstraint
+
+PreintegratedImuMeasurements *imu_preintegrated_;
+
+
+int main()
+{
+    
+}

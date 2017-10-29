dc2347235cdb97da6824613948c371575a753fc7
steve <551619855@qq.com>
2017-10-29 16:34:12 +0800

    zupt for JY imu , good parameters. JYImuMag With good result at parameter: ./cmake-build-debug/JYImuMag 0.000001 0.01 0.02 9.6 0.1 0.2

--- JYImuMag.cpp
+++ JYImuMag.cpp
@@ -97,11 +97,12 @@ int main(int argc, char *argv[]) {
      */
 //    std::string dir_name = "/home/steve/Data/AttitudeIMU/";
 //    std::string dir_name = "/home/steve/Code/Mini_IMU/Scripts/IMUWB/91/";
-    std::string dir_name = "/home/steve/Data/IU/92/";
+//    std::string dir_name = "/home/steve/Data/IU/92/";
+    std::string dir_name = "/home/steve/Data/II/20/";
 
 
 //    CppExtent::CSVReader imu_data_reader(dir_name + "ImuData.csv");
-    CppExtent::CSVReader imu_data_reader(dir_name + "imu.txt");
+    CppExtent::CSVReader imu_data_reader(dir_name + "imu2.txt");
     Eigen::MatrixXd imudata;
     imudata.resize(imu_data_reader.GetMatrix().GetRows(),
                    imu_data_reader.GetMatrix().GetCols());
@@ -617,11 +618,11 @@ int main(int argc, char *argv[]) {
                + "g:" + std::to_string(gravity) + "s_mag_att:" + std::to_string(smag_attitude) +
                "s_g_att:" + std::to_string(sgravity_attitude) + "initial_heading:" + std::to_string(initial_heading));
 
-//    plt::save("img-sv:" + std::to_string(sv) + "sa:" + std::to_string(sa) + "-sg:" +
-//              std::to_string(sg)
-//              + "g:" + std::to_string(gravity) + "s_mag_att:" + std::to_string(smag_attitude) +
-//              "s_g_att:" + std::to_string(sgravity_attitude) + "initial_heading:" + std::to_string(initial_heading) +
-//              ".png");
+    plt::save("img-sv:" + std::to_string(sv) + "sa:" + std::to_string(sa) + "-sg:" +
+              std::to_string(sg)
+              + "g:" + std::to_string(gravity) + "s_mag_att:" + std::to_string(smag_attitude) +
+              "s_g_att:" + std::to_string(sgravity_attitude) + "initial_heading:" + std::to_string(initial_heading) +
+              ".png");
 
 
 
@@ -629,7 +630,7 @@ int main(int argc, char *argv[]) {
 //    plt::plot(ay);
 //    plt::plot(az);
 //    plt::plot(zupt_v);
-    plt::show();
+//    plt::show();
 
 
     return 0;

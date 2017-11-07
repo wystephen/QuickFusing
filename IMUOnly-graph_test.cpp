7654c9bef63c9c7e92f8955bdd5b2c993b4742e2
steve <551619855@qq.com>
2017-11-07 10:49:34 +0800

    double-feet constrainted zupt. added relative Mag constraint.

--- IMUOnly-graph_test.cpp
+++ IMUOnly-graph_test.cpp
@@ -394,7 +394,7 @@ int main(int argc, char *argv[]) {
                  iter != key_info_mag.end();
                  iter++) {
                 if ((iter->data_vec_.block(7, 0, 3, 1).transpose() -
-                     imudata.block(index, 7, 1, 3)).norm() < 30) {
+                     imudata.block(index, 7, 1, 3)).norm() < 10) {
                     std::cout << "src :" << iter->data_vec_.block(7, 0, 3, 1) << std::endl;
                     std::cout << "target :" << imudata.block(index, 7, 1, 3).transpose() << std::endl;
                     auto *mag_edge = new RelativeMagEdge(iter->data_vec_.block(7, 0, 3, 1),

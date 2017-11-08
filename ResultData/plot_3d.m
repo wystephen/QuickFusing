4402347199cd43b06325168d381db2c38efae494
steve <551619855@qq.com>
2017-11-08 17:16:35 +0800

    double-feet constrainted zupt. added relative Mag constraint.

--- ResultData/plot_3d.m
+++ ResultData/plot_3d.m
@@ -1,8 +1,13 @@
 clear;
-figure(1);
+figure(1);grid on;hold on;
 test=load('test.txt');
 save_trace = test;
 plot3(save_trace(:,1),save_trace(:,2),save_trace(:,3),'*r-');
+the_line = zeros(2,3);
+the_line(1,:) = save_trace(1,:);
+the_line(2,:) = save_trace(length(save_trace),:);
+plot3(the_line(:,1),the_line(:,2),the_line(:,3),'b-');
+% line([save_trace(1,1) save_trace(-1,1)],[save_trace(1,2) save_trace(-1,2)],[save_trace(1,3) save_trace(-1,3));
 grid on;hold on;
 axis equal;
 

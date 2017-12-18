096c77490805eee86c20a48820d30e85baea5584
steve <551619855@qq.com>
2017-12-18 18:04:25 +0800

    DCM-FFT start~

--- Scripts/NewUwbDataPreprocess.py
+++ Scripts/NewUwbDataPreprocess.py
@@ -30,8 +30,22 @@ import matplotlib.pyplot as plt
 import array
 
 class NewUwbDataPre:
-    def __init__(self):
+    def __init__(self,mac_pose_file_name):
         print(self.__class__)
+        mac_pose_file = open(mac_pose_file_name)
+        self.mac_dic  = dict()
+        for line  in mac_pose_file.readlines():
+            self.mac_dic[line.split(',')[0]] = \
+                (float(line.split(',')[1]),
+                 float(line.split(',')[2]))
+
+        for key in mac_dic:
+            print(key,
+                  self.mac_dic.get(key)[0],
+                  self.mac_dic.get(key)[1])
+
+
+
 
 
 
@@ -46,7 +60,7 @@ if __name__ == '__main__':
         mac_dic[line.split(',')[0]] = (float(line.split(',')[1]),float(line.split(',')[2]))
 
     for key in mac_dic:
-        print(key,mac_dic.get(key))
+        print(key,mac_dic.get(key)[0],mac_dic.get(key)[1])
 
 
 

c3475287eaf79824931821964a6cf5748aa13b76
steve <551619855@qq.com>
2017-12-18 18:02:11 +0800

    DCM-FFT start~

--- Scripts/NewUwbDataPreprocess.py
+++ Scripts/NewUwbDataPreprocess.py
@@ -31,3 +31,22 @@ import array
 
 class NewUwbDataPre:
     def __init__(self):
+        print(self.__class__)
+
+
+
+if __name__ == '__main__':
+    dir_name = '/home/steve/Data/NewIU/'
+    num_data = 10
+
+
+    mac_pose_file = open(dir_name+'MacPose.csv')
+    mac_dic  = dict()
+    for line  in mac_pose_file.readlines():
+        mac_dic[line.split(',')[0]] = (float(line.split(',')[1]),float(line.split(',')[2]))
+
+    for key in mac_dic:
+        print(key,mac_dic.get(key))
+
+
+

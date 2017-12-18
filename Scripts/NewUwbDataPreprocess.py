f743e9f5c65918448b0f97ea71e122727dcff3f0
steve <551619855@qq.com>
2017-12-18 18:10:10 +0800

    DCM-FFT start~

--- Scripts/NewUwbDataPreprocess.py
+++ Scripts/NewUwbDataPreprocess.py
@@ -22,45 +22,47 @@
          佛祖保佑       永无BUG 
 '''
 
-
 import os
 import re
 import numpy as np
 import matplotlib.pyplot as plt
 import array
 
+
 class NewUwbDataPre:
-    def __init__(self,mac_pose_file_name):
+
+    def __init__(self, data_file_dir, mac_pose_file_name):
+        '''
+
+        :param data_file_dir: directory of data file.
+
+        :param mac_pose_file_name: i.e. /dir/to/file/file_name.csv
+        '''
         print(self.__class__)
         mac_pose_file = open(mac_pose_file_name)
-        self.mac_dic  = dict()
-        for line  in mac_pose_file.readlines():
+        self.mac_dic = dict()
+        for index, line in enumerate(mac_pose_file.readlines()):
             self.mac_dic[line.split(',')[0]] = \
-                (float(line.split(',')[1]),
+                (int(index),
+                 float(line.split(',')[1]),
                  float(line.split(',')[2]))
 
-        for key in mac_dic:
+        for key in self.mac_dic:
             print(key,
                   self.mac_dic.get(key)[0],
-                  self.mac_dic.get(key)[1])
-
-
-
-
+                  self.mac_dic.get(key)[1],
+                  self.mac_dic.get(key)[2])
 
 
 if __name__ == '__main__':
     dir_name = '/home/steve/Data/NewIU/'
     num_data = 10
 
-
-    mac_pose_file = open(dir_name+'MacPose.csv')
-    mac_dic  = dict()
-    for line  in mac_pose_file.readlines():
-        mac_dic[line.split(',')[0]] = (float(line.split(',')[1]),float(line.split(',')[2]))
-
-    for key in mac_dic:
-        print(key,mac_dic.get(key)[0],mac_dic.get(key)[1])
-
-
-
+    # mac_pose_file = open(dir_name + 'MacPose.csv')
+    # mac_dic = dict()
+    # for index,line in enumerate(mac_pose_file.readlines()):
+    #     mac_dic[line.split(',')[0]] = (int(index),float(line.split(',')[1]), float(line.split(',')[2]))
+    #
+    # for key in mac_dic:
+    #     print(key, mac_dic.get(key)[0], mac_dic.get(key)[1])
+    nudp = NewUwbDataPre(dir_name + str(num_data) + '/', dir_name + 'MacPose.csv')
